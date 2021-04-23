5bff7e18c04a44a1427ca4db8f37407ed1487b24
RamanSlinka <romuch1986@gmail.com>
2021-04-23 20:49:11 +0300

    homework-#1 homework-#2(started)

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -5,7 +5,7 @@ import Message from "./Message";
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Some Name',
-    message: 'some text',
+    message: 'some text ',
     time: '22:00',
 }
 const messageDataAlternative = {
